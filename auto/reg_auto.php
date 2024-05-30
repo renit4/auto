5c4e1643c6c8037c7afb7f890387dbdb7eaebf70
renit4 <renatitagomez5@gmail.com>
2024-05-30 20:42:11 -0300

    .

--- auto/reg_auto.php
+++ auto/reg_auto.php
@@ -1,28 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Autos</title>
-    <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-    crossorigin="anonymous"
-    />
-</head>
-<body  class="p-3 text-primary-emphasis bg-primary-subtle border-primary-subtle ">
-<script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"
-    ></script>
-
-
-
-
+<?php
+    include("../conexion.php");
     
+    $mar = $_POST["marca"];
+    $mod = $_POST["mod"];
+    $color = $_POST["color"];
+    $precio = $_POST["precio"];
+    $cli = $_POST["cod"];
 
+    $sql = "INSERT INTO `auto`(`marca`, `modelo`, `color`, `pventa`, `cod_cliente`) 
+            VALUES ('$mar','$mod','$color',$precio,$cli)";
 
-</body>
-</html>
+    $res = mysqli_query($con,$sql);
+    if($res==true){
+        echo"
+        <script>
+          alert('El auto ha sido registrado correctamente! ');
+          window.location.href = '../index.html';
+        </script>
+        ";
+    }
+    else{
+        echo"
+        <script>
+          alert('Algo salio mal :( ');
+          window.location.href = '../index.html';
+        </script>
+        ";
+    }
+
+?>
