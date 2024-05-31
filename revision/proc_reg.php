38d6f098a68dd99dc670051f30d1ff751ae597fd
renit4 <145850245+renit4@users.noreply.github.com>
2024-05-31 17:47:07 -0300

    .

--- revision/proc_reg.php
+++ revision/proc_reg.php
@@ -1,8 +1,28 @@
 <?php
-include("../conexion.php");
+    include("../conexion.php");
 
+    $fing = $_POST["fing"];
+    $fegre = $_POST["fegre"];
+    $est = $_POST["est"];
+    $cfiltro = $_POST["cfiltro"];
+    $caceite = $_POST["caceite"];
+    $cfreno = $_POST["cfreno"];
+    $desc = $_POST["desc"];
+    $cauto = $_POST["cauto"];
 
+    $sql = "INSERT INTO `revision`(`fingreso`,`fegreso`,`estado`,`cambio_filtro`,`cambio_aceite`,`cambio_freno`,`descripcion`,`cod_auto`)
+                VALUES ('$fing','$fegre','$est',$cfiltro,$caceite,$cfreno,'$desc',$cauto)";
+    $res = mysqli_query($con,$sql);
 
-
+    if($res==true){
+        echo "<script> alert ('Ha salido excelente!');
+            window.location.href = '../index.html';
+        </script>";
+    }
+    else{
+        echo "<script> alert ('salio mal :(');
+            window.location.href = '../index.html';
+        </script>";
+    }
 
 ?>
